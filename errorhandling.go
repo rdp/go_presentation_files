d7d3c83afc003833494ba6df69c2d9d464b74556
rogerdpack <roger.pack@familysearch.org>
2014-10-13 11:13:28 -0600

    tweak error handling

--- errorhandling.go
+++ errorhandling.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
     bytesWritten, err := fmt.Println("what if this fails?")
-    fmt.Printf("did it fail %v?", bytesWritten, err)
-    // bytesWritten, _ := fmt.Println("what if this fails?")
+    fmt.Printf("did it fail bytesWritten= %v err=%v?", bytesWritten, err)
+    bytesWritten, _ = fmt.Println("")
+    fmt.Printf("bytesWritten = %d", bytesWritten)
 }
